f35bde122de938c1a19bd59100b588112825b2e0
dstrzykalski <dstrzykalski@yahoo.com>
2024-12-06 23:59:09 -0500

    Updated Script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById('poke-container')
-const pokemon_count = 150
+const pokemon_count = 251
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -14,5 +14,23 @@ const colors = {
 	psychic: '#eaeda1',
 	flying: '#F5F5F5',
 	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+	normal: '#F5F5F5',
+    steel: '#D6D6E0',
+    ghost: '#5f5f5f',
+    ice: '#96D9D6',
+    dark: '#707070'
+}
+
+const fetchPokemons = async () => {
+    for (let i = 1; i <= pokemon_count; i++) {
+        await getPokemon(i)
+    }
+}
+
+const getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data)
+    createPokemonCard(data)
 }
